efd84121ed5ee50426d586b7d498cf45e7253425
Kevin Denilson cax <130245547+KevinCax@users.noreply.github.com>
2025-04-15 22:59:56 -0600

    Add files via upload
    Commit Initial

---
+++ src/main/java/com/producto/servicioProducto/ServicioProductoApplication.java
@@ -0,0 +1,13 @@
+package com.producto.servicioProducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/producto/servicioProducto/ServicioProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto.servicioProducto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
